c51f1e2662da6ae69be269fd6a62529c0cadb45a
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2025-11-22 17:36:36 -0500

    Change vite config as we will be using a new root domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Change 'gaza-responde' to your actual GitHub repository name
-  // If your repo is https://github.com/user/my-book-site, this should be '/my-book-site/'
-  base: '/gaza-responde/', 
+  base: '/', 
 });
